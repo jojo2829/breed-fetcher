9855b7e0d8f6ebdebcf7bd1345b3ad8365272d64
jojo2829 <joulinli@gmail.com>
2021-06-04 01:47:13 +0000

    refector breedFetcher and added mocha chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,29 +1,32 @@
 const request = require("request");
-const args = process.argv.slice(2);
 
-function makeRequest(petid, callback) {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${petid}`, (error, response, body) => {
+function fetchBreedDescription(breedName, callback) {
+  makeRequest(breedName, callback);
+ 
+}
+
+function makeRequest(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
 
     if (error) {
-      console.log(error);
+      callback(error, null);
       return;
     }
 
-    if (body === "[]") {
-      console.log("cat not found");
+    const data = JSON.parse(body);
+
+    if (data.length === 0) {
+      callback("cat not found");
       return;
     }
 
-    callback(body);
+    const desc = data[0].description;
+
+    callback(error, desc);
  
   });
 }
 
-function displayData(body) {
-  const data = JSON.parse(body);
-  const description = data[0].description;
-  //console.log("data:", data);
-  console.log("description: " + description);
-}
-
-makeRequest(args[0], displayData);
+module.exports = {
+  fetchBreedDescription
+};
